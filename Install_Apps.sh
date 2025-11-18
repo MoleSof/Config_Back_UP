54b30256dd12f51a8c2d41eba690dcd00a40d72c
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-17 22:58:28 -0700

    Create Install_Apps.sh

---
+++ Install_Apps.sh
@@ -0,0 +1,6 @@
+
+
+
+#install dvd audio tools
+$wget https://downloads.sourceforge.net/project/dvd-audio/dvda-author/dvda-author-dev/linux%20%2B%20source%20%28amd64%20build%29/dvda-author-21.10-ubuntu-impish-amd64.deb?ts=gAAAAABpGw45rr1i7WecBwS3RywanZCAzFKh2gnC0s2IZi13dkRoinD1l3ZYnGjpSrQaK_47MSxhc4bEumuB7JR1dmmfxAKK3Q%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdvd-audio%2Ffiles%2Flatest%2Fdownload
+$sudo dpkg install
